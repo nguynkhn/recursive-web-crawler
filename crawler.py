e1413e562dfaa97cd5fc6029a35c61abbd4b66c4
nguynkhn <nguynkhan479@gmail.com>
2024-12-14 20:02:49 +0700

    rewrite

--- crawler.py
+++ crawler.py
@@ -1,87 +1,94 @@
+from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
+import requests
 import os
 import sys
-import requests
-from bs4 import BeautifulSoup
 
-queue = [sys.argv[1]]
-visited = {}
-outdir = "output"
-timeout = 60
+class WebCrawler:
+    def __init__(self, start_url, **kwargs):
+        uri = self.parse_url(start_url)
+        self.hostname = uri.netloc
 
-def write_file(url, content=None):
-    if not content:
-        if url in visited:
-            content = visited[url]
-        else:
-            try:
-                res = requests.get(url, timeout=timeout)
-                if not res.ok:
-                    print(f"Requesting {url} failed")
-                    return
+        self.visited = set()
+        self.downloaded = {}
+        self.queue = { uri.geturl() }
 
-                content = res.content
-            except:
-                print(f"Requesting {url} failed")
-                return
+        self.outdir = kwargs.get("outdir", "output")
+        self.timeout = kwargs.get("timeout", 10)
 
-    visited[url] = content
+    def parse_url(self, url):
+        return urlparse(url.lower())._replace(query="", fragment="")
 
-    filename = os.path.join(outdir, urlparse(url).path[1:])
-    dirname = os.path.dirname(filename)
+    def validate_url(self, url, path):
+        uri = self.parse_url(path)
 
-    os.makedirs(dirname, exist_ok=True)
-    with open(filename, "wb") as file:
-        file.write(content)
-
-def crawl(url):
-    res = requests.get(url, timeout=timeout)
-    if not res.ok:
-        print(f"Requesting {url} failed")
-        return
-
-    write_file(url, res.content)
-
-    print(f"Crawling {url}...")
-    soup = BeautifulSoup(res.text, "html.parser")
-
-    hostname = urlparse(url).netloc
-    def validate_url(path):
-        if not path:
-            return False
-
-        uri = urlparse(path.lower())._replace(query="", fragment="")
         if uri.scheme and uri.scheme not in ["http", "https"]:
             return False
 
         if uri.netloc:
-            if uri.netloc != hostname:
-                # don't go too far away from our hostname
+            if uri.netloc != self.hostname:
                 return False
         else:
-            # relative path
             return urljoin(url, uri.geturl())
 
         return uri.geturl()
 
-    # images, audio, gif, css, ...
-    media_elems = soup.find_all(src=True) + soup.find_all("link", rel="stylesheet")
-    for elem in media_elems:
-        src = validate_url(elem.get("src") or elem.get("href"))
-        if not src:
-            continue
+    def download(self, url):
+        if url in self.downloaded:
+            return self.downloaded[url]
 
-        write_file(src)
+        try:
+            res = requests.get(url, timeout=self.timeout)
+            content = res.content
 
-    href_elems = soup.find_all(href=True)
-    for elem in href_elems:
-        href = validate_url(elem.get("href"))
-        if not href or href in visited:
-            continue
+            uri = self.parse_url(url)
+            url = uri.netloc + uri.path
+            filename = os.path.normpath(os.path.join(self.outdir, url))
+            dirname = os.path.dirname(filename)
 
-        queue.append(href)
+            os.makedirs(dirname, exist_ok=True)
+            with open(filename, "wb") as file:
+                file.write(content)
+
+            assert res.ok
+            self.downloaded[url] = content
+            return content
+        except:
+            print(f"Requesting {url} failed!")
+            return None
+
+    def start(self):
+        while len(self.queue) > 0:
+            url = self.queue.pop()
+            self.visited.add(url)
+            print(f"Crawling {url}...")
+
+            content = self.download(url)
+            if not content:
+                continue
+
+            soup = BeautifulSoup(content, "html.parser")
+
+            # find all hrefs
+            href_tags = soup.find_all(href=True)
+            for href_tag in href_tags:
+                href_url = self.validate_url(url, href_tag.get("href"))
+                if not href_url or href_url in self.visited:
+                    continue
+
+                self.queue.add(href_url)
+
+            # find all media (image, css, gif, ...)
+            # javascript code too i guess :P
+            media_tags = soup.find_all(src=True) + soup.find_all("link", rel="stylesheet")
+            for media_tag in media_tags:
+                media_url = self.validate_url(url, media_tag.get("src") or media_tag.get("href"))
+                if not media_url:
+                    continue
+
+                self.download(media_url)
 
 if __name__ == "__main__":
-    while len(queue) > 0:
-        url = queue.pop()
-        crawl(url)
+    url = sys.argv[1]
+    crawler = WebCrawler(url)
+    crawler.start()
