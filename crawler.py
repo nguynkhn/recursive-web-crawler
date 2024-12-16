e61860e70e763571a73f0e953cd3fd476d3941e8
nguynkhn <nguynkhan479@gmail.com>
2024-12-16 21:56:39 +0700

    add argument parser & depth

--- crawler.py
+++ crawler.py
@@ -1,99 +1,123 @@
+from urllib.parse import urlsplit, urljoin
 from bs4 import BeautifulSoup
-from urllib.parse import urlparse, urljoin
-import requests
 import os
-import sys
+import requests
+import argparse
+
+parse_uri = lambda url: urlsplit(url.lower())._replace(query='', fragment='')
+get_uri_paths = lambda uri: uri.path.strip('/').split('/')[:-1]
 
 class WebCrawler:
-    def __init__(self, start_url, **kwargs):
-        uri = self.parse_url(start_url)
-        self.hostname = uri.netloc
+    def __init__(self, args):
+        self.args = args
 
-        self.visited = set()
-        self.downloaded = {}
-        self.queue = { uri.geturl() }
+        self.uri = parse_uri(self.args.url)
+        self.base_paths = get_uri_paths(self.uri)
 
-        self.outdir = kwargs.get("outdir", "output")
-        self.timeout = kwargs.get("timeout", 10)
+        self.queue = set()
+        self.cache = {}
 
-    def parse_url(self, url):
-        return urlparse(url.lower())._replace(query="", fragment="")
+    def validate_uri(self, uri):
+        return not uri.scheme or uri.scheme in ['http', 'https'] \
+            and uri.netloc == self.uri.netloc
 
-    def validate_url(self, url, path):
-        uri = self.parse_url(path)
+    def check_depth(self, uri):
+        test_paths = get_uri_paths(uri)
+        base_level, test_level = len(self.base_paths), len(test_paths)
 
-        if uri.scheme and uri.scheme not in ["http", "https"]:
+        if self.args.depth < 0:
+            match_len = max(base_level + self.args.depth, 0)
+            return self.base_paths[:match_len] == test_paths[:match_len]
+
+        if test_level < base_level:
+            # test url can't be shorter
             return False
 
-        if uri.netloc:
-            if uri.netloc != self.hostname:
-                return False
-        else:
-            return urljoin(url, uri.geturl())
+        return test_paths[:base_level] == self.base_paths \
+            and test_level - base_level <= self.args.depth
 
-        return uri.geturl()
-
-    def download(self, url):
-        if url in self.downloaded:
-            return self.downloaded[url]
+    def fetch_and_save(self, uri):
+        if uri in self.cache:
+            return self.cache[uri]
 
         try:
-            res = requests.get(url, timeout=self.timeout)
-            content = res.content
+            res = requests.get(uri.geturl(), timeout=self.args.timeout)
 
-            uri = self.parse_url(url)
-            fileurl = uri.netloc + uri.path
-            filename = os.path.normpath(os.path.join(self.outdir, fileurl))
-            dirname = os.path.dirname(filename)
+            filename = os.path.normpath(uri.netloc + uri.path)
+            filepath = os.path.join(self.args.output, filename)
+            dirpath = os.path.dirname(filepath)
 
-            os.makedirs(dirname, exist_ok=True)
-            with open(filename, "wb") as file:
-                file.write(content)
+            os.makedirs(dirpath, exist_ok=True)
+            with open(filepath, 'wb') as f:
+                f.write(res.content)
 
-            assert res.ok
-            self.downloaded[url] = content
-            return content
+            if res.ok:
+                self.cache[uri] = res
+                return res
         except:
-            print(f"Requesting {url} failed!")
-            return None
+            pass
 
-    def start(self):
-        while len(self.queue) > 0:
-            url = self.queue.pop()
-            self.visited.add(url)
-            print(f"Crawling {url}...")
+        return None
 
-            content = self.download(url)
-            if not content:
+    def crawl(self, uri):
+        res = self.fetch_and_save(uri)
+        if not res:
+            print(f"Requesting {uri.geturl()} failed!")
+            return
+
+        if not res.headers['Content-Type'].startswith('text/html'):
+            # not html file... skip
+            return
+
+        soup = BeautifulSoup(res.content, 'html.parser')
+
+        # find all hrefs
+        href_tags = soup.find_all(href=True)
+        for href_tag in href_tags:
+            href_url = urljoin(uri.geturl(), href_tag.get('href'))
+            href_uri = parse_uri(href_url)
+            if not self.validate_uri(href_uri) or href_uri in self.cache \
+                or not self.check_depth(href_uri):
                 continue
 
-            soup = BeautifulSoup(content, "html.parser")
+            self.queue.add(href_uri)
 
-            # find all hrefs
-            href_tags = soup.find_all(href=True)
-            for href_tag in href_tags:
-                href_url = self.validate_url(url, href_tag.get("href"))
-                if not href_url or href_url in self.visited:
-                    continue
+        # find all media (image, gif, ...)
+        # js code too i guess
+        media_tags = soup.find_all(src=True)
+        for media_tag in media_tags:
+            media_url = urljoin(uri.geturl(), media_tag.get('src'))
+            media_uri = parse_uri(media_url)
+            if not self.validate_uri(media_uri):
+                continue
 
-                # css probably
-                if href_tag.name == "link":
-                    self.download(href_url)
-                    continue
+            self.fetch_and_save(media_uri)
 
-                self.queue.add(href_url)
+    def start(self):
+        self.queue.add(self.uri)
 
-            # find all media (image, gif, ...)
-            # javascript code too i guess :P
-            media_tags = soup.find_all(src=True)
-            for media_tag in media_tags:
-                media_url = self.validate_url(url, media_tag.get("src"))
-                if not media_url:
-                    continue
+        while len(self.queue) > 0:
+            uri = self.queue.pop()
 
-                self.download(media_url)
+            print(f"Crawling {uri.geturl()}...")
+            self.crawl(uri)
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    crawler = WebCrawler(url)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', type=str, help='starting url')
+    parser.add_argument(
+        '-o', '--output', type=str, default='output/',
+        help='output directory (default: output/)',
+    )
+    parser.add_argument(
+        '-t', '--timeout', type=int, default=10,
+        help='request\'s timeout in seconds (default: 10)',
+    )
+    parser.add_argument(
+        '-d', '--depth', type=int, default=0,
+        help='maximum depth of url (default: 0)',
+    )
+
+    args = parser.parse_args()
+    crawler = WebCrawler(args)
     crawler.start()
